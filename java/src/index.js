63a94f95d5f3c2ab7d2e26348a5e0b2be5c56c6b
aarthiperiyasamy1997 <aarthiperiyasamy1997@gmail.com>
2022-03-03 11:42:11 +0530

    project created and executed

---
+++ java/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+ReactDom.render(
+    <>
+       <h1>java fullstack</h1>
+       <h2>Aarthi</h2>
+    </>
+
+  ,document.getElementById("root")  
+)
