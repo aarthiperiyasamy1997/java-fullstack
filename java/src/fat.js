cd7b0973c885b88dfb08803646c4fd69266bf3f9
aarthiperiyasamy1997 <aarthiperiyasamy1997@gmail.com>
2022-03-03 13:55:43 +0530

    yes

--- java/src/fat.js
+++ java/src/fat.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Aarthi=()=>{
     return(
         <ul>
-            <li>python developer</li>
+            
             <li>java developer</li>
         </ul>
     )  
