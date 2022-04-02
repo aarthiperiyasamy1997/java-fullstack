31e1528d1b38e192f6423571b52281bc0535e77d
aarthiperiyasamy1997 <aarthiperiyasamy1997@gmail.com>
2022-04-02 14:00:56 +0530

    create table

---
+++ POC1BackEnd/src/main/java/backend/poc/first/POC1BackEnd/Resources.java
@@ -0,0 +1,73 @@
+package backend.poc.first.POC1BackEnd;
+
+
+import java.util.Arrays;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Resources {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int uid;
+	private String uname;
+	private String uplace;
+	private int usalary;
+	private String[] uskills;
+	
+	public Resources() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Resources [uid=" + uid + ", uname=" + uname + ", uplace=" + uplace + ", usalary=" + usalary
+				+ ", uskills=" + Arrays.toString(uskills) + "]";
+	}
+	public Resources(String uname, String uplace, int usalary, String[] uskills) {
+		super();
+		this.uname = uname;
+		this.uplace = uplace;
+		this.usalary = usalary;
+		this.uskills = uskills;
+	}
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getUplace() {
+		return uplace;
+	}
+	public void setUplace(String uplace) {
+		this.uplace = uplace;
+	}
+	public int getUsalary() {
+		return usalary;
+	}
+	public void setUsalary(int usalary) {
+		this.usalary = usalary;
+	}
+	public String[] getUskills() {
+		return uskills;
+	}
+	public void setUskills(String[] uskills) {
+		this.uskills = uskills;
+	}
+	
+	
+	
+	
+
+}
